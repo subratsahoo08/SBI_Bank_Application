62b54609cd199092c97bffd93745df38a85292d2
subratsahoo08 <subrat.sahoo08@yahoo.com>
2025-06-12 04:01:22 +0530

    Update sample3.java

--- sample3.java
+++ sample3.java
@@ -2,7 +2,7 @@ public class MyClass {
   static void myMethod() {
     System.out.println("Welcome to Subrat Devops!");
   }
-  {System.out.printLn("Hello");
+  {System.out.printLn("Hello, this is Subrat");
 
   public static void main(String[] args) {
     myMethod();
